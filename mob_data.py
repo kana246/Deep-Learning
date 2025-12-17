52c7adb3c2a62aec6e9c959e2051997fa09d190a
ポテリング <poteringu77@gmail.com>
2025-12-17 23:54:20 +0900

    Create mob_data.py
    Signed-off-by: ポテリング <poteringu77@gmail.com>

---
+++ mob_data.py
@@ -0,0 +1,247 @@
+# mob_data.py
+
+mobs = {
+    "zombie": {
+        "name": "ゾンビ",
+        "desc": "敵対的なアンデッドモブ",
+        "category": "敵対的モブ",
+        "aliases": [
+            "ゾンビ", "ぞんび", "zombie", "アンデッド", "あんでっど"
+        ],
+        "id": {
+            "統合版": "zombie",
+            "Java版": "minecraft:zombie"
+        }
+    },
+    "skeleton": {
+        "name": "スケルトン",
+        "desc": "弓を使う敵対的モブ",
+        "category": "敵対的モブ",
+        "aliases": [
+            "スケルトン", "すけるとん", "skeleton", "骨", "ほね", "がいこつ"
+        ],
+        "id": {
+            "統合版": "skeleton",
+            "Java版": "minecraft:skeleton"
+        }
+    },
+    "creeper": {
+        "name": "クリーパー",
+        "desc": "爆発する敵対的モブ",
+        "category": "敵対的モブ",
+        "aliases": [
+            "クリーパー", "くりーぱー", "creeper", "爆発", "ばくはつ", "匠", "たくみ"
+        ],
+        "id": {
+            "統合版": "creeper",
+            "Java版": "minecraft:creeper"
+        }
+    },
+    "spider": {
+        "name": "クモ",
+        "desc": "壁を登る敵対的モブ",
+        "category": "敵対的モブ",
+        "aliases": [
+            "クモ", "くも", "spider", "蜘蛛", "スパイダー"
+        ],
+        "id": {
+            "統合版": "spider",
+            "Java版": "minecraft:spider"
+        }
+    },
+    "enderman": {
+        "name": "エンダーマン",
+        "desc": "ブロックを持ち運ぶ中立的モブ",
+        "category": "中立的モブ",
+        "aliases": [
+            "エンダーマン", "えんだーまん", "enderman", "エンダー", "黒い", "テレポート"
+        ],
+        "id": {
+            "統合版": "enderman",
+            "Java版": "minecraft:enderman"
+        }
+    },
+    "pig": {
+        "name": "ブタ",
+        "desc": "友好的な動物",
+        "category": "友好的モブ",
+        "aliases": [
+            "ブタ", "ぶた", "pig", "豚", "ピッグ"
+        ],
+        "id": {
+            "統合版": "pig",
+            "Java版": "minecraft:pig"
+        }
+    },
+    "cow": {
+        "name": "ウシ",
+        "desc": "友好的な動物",
+        "category": "友好的モブ",
+        "aliases": [
+            "ウシ", "うし", "cow", "牛", "カウ"
+        ],
+        "id": {
+            "統合版": "cow",
+            "Java版": "minecraft:cow"
+        }
+    },
+    "sheep": {
+        "name": "ヒツジ",
+        "desc": "友好的な動物",
+        "category": "友好的モブ",
+        "aliases": [
+            "ヒツジ", "ひつじ", "sheep", "羊", "シープ"
+        ],
+        "id": {
+            "統合版": "sheep",
+            "Java版": "minecraft:sheep"
+        }
+    },
+    "chicken": {
+        "name": "ニワトリ",
+        "desc": "友好的な動物",
+        "category": "友好的モブ",
+        "aliases": [
+            "ニワトリ", "にわとり", "chicken", "鶏", "チキン", "とり", "鳥"
+        ],
+        "id": {
+            "統合版": "chicken",
+            "Java版": "minecraft:chicken"
+        }
+    },
+    "villager": {
+        "name": "村人",
+        "desc": "取引ができる友好的モブ",
+        "category": "友好的モブ",
+        "aliases": [
+            "村人", "むらびと", "villager", "ビレジャー", "商人", "取引"
+        ],
+        "id": {
+            "統合版": "villager",
+            "Java版": "minecraft:villager"
+        }
+    },
+    "iron_golem": {
+        "name": "アイアンゴーレム",
+        "desc": "村を守る友好的モブ",
+        "category": "友好的モブ",
+        "aliases": [
+            "アイアンゴーレム", "あいあんごーれむ", "iron_golem", "鉄ゴーレム", "ゴーレム"
+        ],
+        "id": {
+            "統合版": "iron_golem",
+            "Java版": "minecraft:iron_golem"
+        }
+    },
+    "wolf": {
+        "name": "オオカミ",
+        "desc": "飼いならせる中立的モブ",
+        "category": "中立的モブ",
+        "aliases": [
+            "オオカミ", "おおかみ", "wolf", "狼", "ウルフ", "犬", "いぬ"
+        ],
+        "id": {
+            "統合版": "wolf",
+            "Java版": "minecraft:wolf"
+        }
+    },
+    "cat": {
+        "name": "ネコ",
+        "desc": "飼いならせる友好的モブ",
+        "category": "友好的モブ",
+        "aliases": [
+            "ネコ", "ねこ", "cat", "猫", "キャット"
+        ],
+        "id": {
+            "統合版": "cat",
+            "Java版": "minecraft:cat"
+        }
+    },
+    "horse": {
+        "name": "ウマ",
+        "desc": "乗れる友好的モブ",
+        "category": "友好的モブ",
+        "aliases": [
+            "ウマ", "うま", "horse", "馬", "ホース"
+        ],
+        "id": {
+            "統合版": "horse",
+            "Java版": "minecraft:horse"
+        }
+    },
+    "blaze": {
+        "name": "ブレイズ",
+        "desc": "ネザー要塞にいる敵対的モブ",
+        "category": "敵対的モブ",
+        "aliases": [
+            "ブレイズ", "ぶれいず", "blaze", "炎", "ほのお"
+        ],
+        "id": {
+            "統合版": "blaze",
+            "Java版": "minecraft:blaze"
+        }
+    },
+    "ghast": {
+        "name": "ガスト",
+        "desc": "火の玉を撃つ敵対的モブ",
+        "category": "敵対的モブ",
+        "aliases": [
+            "ガスト", "がすと", "ghast", "白い", "泣く"
+        ],
+        "id": {
+            "統合版": "ghast",
+            "Java版": "minecraft:ghast"
+        }
+    },
+    "wither_skeleton": {
+        "name": "ウィザースケルトン",
+        "desc": "ネザー要塞にいる敵対的モブ",
+        "category": "敵対的モブ",
+        "aliases": [
+            "ウィザースケルトン", "うぃざーすけるとん", "wither_skeleton", "黒いスケルトン"
+        ],
+        "id": {
+            "統合版": "wither_skeleton",
+            "Java版": "minecraft:wither_skeleton"
+        }
+    },
+    "ender_dragon": {
+        "name": "エンダードラゴン",
+        "desc": "ジ・エンドのボス",
+        "category": "ボス",
+        "aliases": [
+            "エンダードラゴン", "えんだーどらごん", "ender_dragon", "ドラゴン", "ボス"
+        ],
+        "id": {
+            "統合版": "ender_dragon",
+            "Java版": "minecraft:ender_dragon"
+        }
+    },
+    "wither": {
+        "name": "ウィザー",
+        "desc": "召喚できるボス",
+        "category": "ボス",
+        "aliases": [
+            "ウィザー", "うぃざー", "wither", "三つ首", "ボス"
+        ],
+        "id": {
+            "統合版": "wither",
+            "Java版": "minecraft:wither"
+        }
+    },
+    "bat": {
+        "name": "コウモリ",
+        "desc": "洞窟にいる友好的モブ",
+        "category": "友好的モブ",
+        "aliases": [
+            "コウモリ", "こうもり", "bat", "蝙蝠"
+        ],
+        "id": {
+            "統合版": "bat",
+            "Java版": "minecraft:bat"
+        }
+    }
+}
+
+# カテゴリ一覧
+categories = ["敵対的モブ", "友好的モブ", "中立的モブ", "ボス"]
