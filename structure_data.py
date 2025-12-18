9fca0aefe79e103cb8a5959a19ade292ea32b3b9
ポテリング <poteringu77@gmail.com>
2025-12-18 09:29:46 +0900

    Create structure_data.py
    Signed-off-by: ポテリング <poteringu77@gmail.com>

---
+++ structure_data.py
@@ -0,0 +1,252 @@
+# structure_data.py
+
+structures = {
+    "ancient_city": {
+        "name": "古代都市",
+        "desc": "ディープダークバイオームに生成される構造物",
+        "category": "地下構造物",
+        "aliases": [
+            "古代都市", "こだいとし", "ancient_city", "古代", "ディープダーク",
+            "ウォーデン", "うぉーでん", "深層", "しんそう"
+        ],
+        "id": {
+            "統合版": "ancient_city",
+            "Java版": "minecraft:ancient_city"
+        }
+    },
+    "village": {
+        "name": "村",
+        "desc": "村人が住む構造物",
+        "category": "地上構造物",
+        "aliases": [
+            "村", "むら", "village", "ビレッジ", "村人の村", "むらびと"
+        ],
+        "id": {
+            "統合版": "village",
+            "Java版": "minecraft:village"
+        }
+    },
+    "pillager_outpost": {
+        "name": "ピリジャーの前哨基地",
+        "desc": "ピリジャーが住む塔",
+        "category": "地上構造物",
+        "aliases": [
+            "前哨基地", "ぜんしょうきち", "pillager_outpost", "ピリジャー",
+            "ぴりじゃー", "略奪者", "りゃくだつしゃ", "塔", "とう"
+        ],
+        "id": {
+            "統合版": "pillager_outpost",
+            "Java版": "minecraft:pillager_outpost"
+        }
+    },
+    "mansion": {
+        "name": "森の洋館",
+        "desc": "ダークフォレストに生成される大きな建物",
+        "category": "地上構造物",
+        "aliases": [
+            "森の洋館", "もりのようかん", "mansion", "洋館", "ようかん",
+            "マンション", "エヴォーカー", "ヴィンディケーター"
+        ],
+        "id": {
+            "統合版": "mansion",
+            "Java版": "minecraft:mansion"
+        }
+    },
+    "monument": {
+        "name": "海底神殿",
+        "desc": "海底に生成される構造物",
+        "category": "海洋構造物",
+        "aliases": [
+            "海底神殿", "かいていしんでん", "monument", "神殿", "しんでん",
+            "ガーディアン", "がーでぃあん", "エルダーガーディアン"
+        ],
+        "id": {
+            "統合版": "monument",
+            "Java版": "minecraft:monument"
+        }
+    },
+    "mineshaft": {
+        "name": "廃坑",
+        "desc": "地下に生成される坑道",
+        "category": "地下構造物",
+        "aliases": [
+            "廃坑", "はいこう", "mineshaft", "鉱山", "こうざん",
+            "坑道", "こうどう", "マインシャフト"
+        ],
+        "id": {
+            "統合版": "mineshaft",
+            "Java版": "minecraft:mineshaft"
+        }
+    },
+    "stronghold": {
+        "name": "要塞",
+        "desc": "エンドポータルがある地下構造物",
+        "category": "地下構造物",
+        "aliases": [
+            "要塞", "ようさい", "stronghold", "エンド要塞", "えんどようさい",
+            "ポータル", "ぽーたる", "エンドポータル"
+        ],
+        "id": {
+            "統合版": "stronghold",
+            "Java版": "minecraft:stronghold"
+        }
+    },
+    "fortress": {
+        "name": "ネザー要塞",
+        "desc": "ネザーに生成される要塞",
+        "category": "ネザー構造物",
+        "aliases": [
+            "ネザー要塞", "ねざーようさい", "fortress", "要塞", "ようさい",
+            "ブレイズ", "ぶれいず", "ウィザースケルトン"
+        ],
+        "id": {
+            "統合版": "fortress",
+            "Java版": "minecraft:fortress"
+        }
+    },
+    "bastion_remnant": {
+        "name": "砦の遺跡",
+        "desc": "ネザーに生成されるピグリンの構造物",
+        "category": "ネザー構造物",
+        "aliases": [
+            "砦の遺跡", "とりでのいせき", "bastion_remnant", "砦", "とりで",
+            "バスティオン", "ばすてぃおん", "ピグリン", "ぴぐりん"
+        ],
+        "id": {
+            "統合版": "bastion_remnant",
+            "Java版": "minecraft:bastion_remnant"
+        }
+    },
+    "end_city": {
+        "name": "エンドシティ",
+        "desc": "ジ・エンドに生成される都市",
+        "category": "エンド構造物",
+        "aliases": [
+            "エンドシティ", "えんどしてぃ", "end_city", "エンドの街",
+            "シュルカー", "しゅるかー", "エリトラ", "えりとら"
+        ],
+        "id": {
+            "統合版": "end_city",
+            "Java版": "minecraft:end_city"
+        }
+    },
+    "desert_pyramid": {
+        "name": "砂漠の寺院",
+        "desc": "砂漠に生成されるピラミッド",
+        "category": "地上構造物",
+        "aliases": [
+            "砂漠の寺院", "さばくのじいん", "desert_pyramid", "ピラミッド",
+            "ぴらみっど", "砂漠", "さばく"
+        ],
+        "id": {
+            "統合版": "desert_pyramid",
+            "Java版": "minecraft:desert_pyramid"
+        }
+    },
+    "jungle_pyramid": {
+        "name": "ジャングルの寺院",
+        "desc": "ジャングルに生成される寺院",
+        "category": "地上構造物",
+        "aliases": [
+            "ジャングルの寺院", "じゃんぐるのじいん", "jungle_pyramid",
+            "ジャングル", "じゃんぐる", "寺院", "じいん"
+        ],
+        "id": {
+            "統合版": "jungle_pyramid",
+            "Java版": "minecraft:jungle_pyramid"
+        }
+    },
+    "swamp_hut": {
+        "name": "湿地の小屋",
+        "desc": "湿地に生成される魔女の小屋",
+        "category": "地上構造物",
+        "aliases": [
+            "湿地の小屋", "しっちのこや", "swamp_hut", "魔女の小屋",
+            "まじょのこや", "魔女", "まじょ", "ウィッチ"
+        ],
+        "id": {
+            "統合版": "swamp_hut",
+            "Java版": "minecraft:swamp_hut"
+        }
+    },
+    "igloo": {
+        "name": "イグルー",
+        "desc": "雪原に生成される氷の家",
+        "category": "地上構造物",
+        "aliases": [
+            "イグルー", "いぐるー", "igloo", "氷の家", "こおりのいえ",
+            "雪原", "せつげん"
+        ],
+        "id": {
+            "統合版": "igloo",
+            "Java版": "minecraft:igloo"
+        }
+    },
+    "shipwreck": {
+        "name": "難破船",
+        "desc": "海や海岸に生成される船",
+        "category": "海洋構造物",
+        "aliases": [
+            "難破船", "なんぱせん", "shipwreck", "船", "ふね",
+            "沈没船", "ちんぼつせん"
+        ],
+        "id": {
+            "統合版": "shipwreck",
+            "Java版": "minecraft:shipwreck"
+        }
+    },
+    "ocean_ruin": {
+        "name": "海底遺跡",
+        "desc": "海底に生成される遺跡",
+        "category": "海洋構造物",
+        "aliases": [
+            "海底遺跡", "かいていいせき", "ocean_ruin", "遺跡", "いせき",
+            "海底", "かいてい"
+        ],
+        "id": {
+            "統合版": "ocean_ruin",
+            "Java版": "minecraft:ocean_ruin"
+        }
+    },
+    "buried_treasure": {
+        "name": "埋もれた宝",
+        "desc": "海岸に埋もれた宝箱",
+        "category": "海洋構造物",
+        "aliases": [
+            "埋もれた宝", "うもれたたから", "buried_treasure", "宝",
+            "たから", "宝箱", "たからばこ"
+        ],
+        "id": {
+            "統合版": "buried_treasure",
+            "Java版": "minecraft:buried_treasure"
+        }
+    },
+    "ruined_portal": {
+        "name": "荒廃したポータル",
+        "desc": "地上やネザーに生成される壊れたポータル",
+        "category": "地上構造物",
+        "aliases": [
+            "荒廃したポータル", "こうはいしたぽーたる", "ruined_portal",
+            "壊れたポータル", "こわれたぽーたる", "ポータル", "ぽーたる"
+        ],
+        "id": {
+            "統合版": "ruined_portal",
+            "Java版": "minecraft:ruined_portal"
+        }
+    },
+    "trail_ruins": {
+        "name": "トレイル遺跡",
+        "desc": "地下に埋もれた遺跡",
+        "category": "地下構造物",
+        "aliases": [
+            "トレイル遺跡", "とれいるいせき", "trail_ruins", "遺跡", "いせき"
+        ],
+        "id": {
+            "統合版": "trail_ruins",
+            "Java版": "minecraft:trail_ruins"
+        }
+    }
+}
+
+# カテゴリ一覧
+categories = ["地上構造物", "地下構造物", "海洋構造物", "ネザー構造物", "エンド構造物"]
